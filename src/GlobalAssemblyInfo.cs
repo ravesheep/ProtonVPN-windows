77a25c18453713fe1ea86fa2c4a32875eba16f01
Ravesheep <ravesheep@pm.me>
2024-02-28 19:05:58 +0100

    Increase app version to 3.2.10.1

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -14,8 +14,8 @@ using System.Runtime.Versioning;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("3.2.10.0")]
-[assembly: AssemblyFileVersion("3.2.10.0")]
+[assembly: AssemblyVersion("3.2.10.1")]
+[assembly: AssemblyFileVersion("3.2.10.1")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyInformationalVersion("$AssemblyVersion")]
 [assembly: SupportedOSPlatform("windows")]
